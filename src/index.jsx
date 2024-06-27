e2e6093439e9906ce520162ccaef1cba94e1ad4d
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2024-06-27 23:39:11 +0300

    add_versel

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import { App } from './components/App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <BrowserRouter>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/goit-react-hw-08/',
   plugins: [react()],
 });
