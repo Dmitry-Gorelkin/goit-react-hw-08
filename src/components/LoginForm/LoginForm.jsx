ff9dc1f65a824a0b1a39d06fb4e483cc7151e7f2
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2024-06-29 16:38:32 +0300

    add_fom-style

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,19 +1,50 @@
+import { useState } from 'react';
+import {
+  FormContrainer,
+  FormLabel,
+  FormInput,
+  FormInputContrainer,
+  FormInputIconVisibilityPassword,
+  FormButton,
+} from '../UI/Form/Form.styled';
+import { LoginFormContrainer } from './LoginForm.styled';
+
 export const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const handlSubmit = e => {
     e.preventDefault();
   };
 
+  const togglePasswordVisible = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div>
-      <form autoComplete="off" onSubmit={handlSubmit}>
-        <label htmlFor="useremail">Email</label>
-        <input type="email" id="useremail" />
+    <LoginFormContrainer>
+      <FormContrainer autoComplete="off" onSubmit={handlSubmit}>
+        <FormLabel>
+          Email
+          <FormInput
+            type="email"
+            name="email"
+            pattern="([A-z0-9_.-]{1,})@([A-z0-9_.-]{1,}).([A-z]{2,8})"
+            required
+          />
+        </FormLabel>
+        <FormLabel>
+          Password
+          <FormInputContrainer>
+            <FormInput type={showPassword ? 'text' : 'password'} name="password" required />
+            <FormInputIconVisibilityPassword
+              onClick={togglePasswordVisible}
+              visible={showPassword}
+            />
+          </FormInputContrainer>
+        </FormLabel>
 
-        <label htmlFor="userpassword">Password</label>
-        <input type="password" id="userpassword" />
-
-        <button type="submit">Log In</button>
-      </form>
-    </div>
+        <FormButton type="submit">Log In</FormButton>
+      </FormContrainer>
+    </LoginFormContrainer>
   );
 };
---
+++ src/components/UI/Form/Form.styled.jsx
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+import { CiRead } from 'react-icons/ci';
+
+export const FormContrainer = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+
+  gap: 5vw;
+
+  @media screen and (min-width: 768px) {
+    gap: 30px;
+
+    width: 400px;
+  }
+
+  @media screen and (min-width: 1200px) {
+  }
+`;
+
+export const FormLabel = styled.label`
+  display: flex;
+
+  flex-direction: column;
+  gap: 10px;
+  text-transform: uppercase;
+`;
+
+export const FormInput = styled.input`
+  display: block;
+  width: 100%;
+
+  outline: none;
+
+  color: ${p => p.theme.colors.textSecondary};
+
+  padding: 10px;
+  padding-right: 30px;
+
+  border: 1px solid ${p => p.theme.colors.backgroundSecondary};
+  border-radius: 4px;
+
+  &:focus {
+    border-color: ${p => p.theme.colors.accent};
+  }
+`;
+
+export const FormInputContrainer = styled.div`
+  display: block;
+  position: relative;
+`;
+
+export const FormInputIconVisibilityPassword = styled(CiRead)`
+  display: block;
+  width: 25px;
+  height: 25px;
+
+  position: absolute;
+
+  cursor: pointer;
+
+  color: ${p => (p.visible ? p.theme.colors.accent : p.theme.colors.textSecondary)};
+
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  right: -5px;
+`;
+
+export const FormButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+
+  padding: 10px;
+
+  cursor: pointer;
+
+  border: none;
+  border-radius: 4px;
+
+  background-color: ${p => p.theme.colors.btn};
+
+  color: ${p => p.theme.colors.textWhite};
+
+  &:hover {
+    background-color: ${p => p.theme.colors.bthHover};
+  }
+`;
