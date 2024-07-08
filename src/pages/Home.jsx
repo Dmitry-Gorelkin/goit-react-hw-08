c57972bf344522526ff0f613b7a74d5fd28ddca0
Дмитрий Горелкин <gorelkind@gmail.com>
2024-07-08 12:02:57 +0300

    add_h1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from 'react-helmet';
+import { Section } from '../components/UI/Section/Section.styled';
 
 const Home = () => {
   return (
@@ -6,7 +7,16 @@ const Home = () => {
       <Helmet>
         <title>Phone book</title>
       </Helmet>
-      <h1>Hello World</h1>
+
+      <h1>Welcome to Your Contact Book</h1>
+      <Section>
+        <p>
+          Welcome to your personal Contact Book! Here, you can effortlessly save and manage all your
+          contacts in one place. Enjoy the convenience of organizing and accessing your contacts
+          anytime, anywhere. Start adding your contacts now and experience the simplicity of our
+          intuitive interface.
+        </p>
+      </Section>
     </>
   );
 };
